c4c5c68807847a223d14b92204341d96e8033704
Griselda Ibarra <reyibme@gmail.com>
2024-11-06 22:38:10 -0600

    Commit formulas terminado

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBuG: True
+    MYSQL_HOST='127.0.0.1'
+    MYSQL_USER:'root'
+    MYSQL_PASSWORD=''
+    MYSQL_DB='API_UTL'
+
+config={
+    'development':DevelopmentConfig
+}
